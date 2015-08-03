e58cdd98ba249d1c9194f50cbe9a34da2c03742b
Sebastian Clausen <github@sebastian-clausen.de>
2015-08-03 14:37:49 +0200

    disable angular2 System.register for normal typescript files

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 Angular2 Typescript
-================
+===================
 
-Transpile Typescript files ending with '.ts' to ES5.
+Transpile Typescript files ending with '.ng.ts' and '.ts' to ES5.
 
-This package uses typescript 1.5 with all the additions to support Angular2 syntax.
+This package uses typescript 1.5.3 with all the additions to support Angular2 syntax.
+
+Angular2 TS Files must have the extension `ng.ts` so the compiler can add the System.register code for angular2.
--- plugin/handler.js
+++ plugin/handler.js
@@ -1,13 +1,24 @@
 var typescript = Npm.require('typescript');
 
 Plugin.registerSourceHandler('ts', function(compileStep) {
-  var output = typescript.transpile(compileStep.read().toString('utf8'), { module : typescript.ModuleKind.System });
-  var moduleName = compileStep.inputPath.replace(/\\/,'/').replace('.ts','');
-  output = output.replace("System.register([",'System.register("'+moduleName+'",[');
+  ngTs = !!compileStep.inputPath.match(new RegExp(/.ng.ts$/i));
+
+  var output, moduleName;
+
+  if (ngTs === true) {
+    output = typescript.transpile(compileStep.read().toString('utf8'), {
+      module: typescript.ModuleKind.System
+    });
+    moduleName = compileStep.inputPath.replace(/\\/, '/').replace('.ng.ts', '');
+    output = output.replace("System.register([", 'System.register("' + moduleName + '",[');
+  } else {
+    moduleName = compileStep.inputPath.replace(/\\/, '/').replace('.ts', '');
+    output = typescript.transpile(compileStep.read().toString('utf8'));
+  }
 
   compileStep.addJavaScript({
-    path : compileStep.inputPath.replace('.ts', '.js'),
-    data : output,
+    path: compileStep.inputPath.replace('.ts', '.js'),
+    data: output,
     sourcePath: compileStep.inputPath
   });
 });
